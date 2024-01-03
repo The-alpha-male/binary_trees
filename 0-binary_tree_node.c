9fd5f6e10a8281d57bbd23795f51f72391f8eb3c
The-alpha-male <ronnymunene@outlook.com>
2024-01-03 12:55:34 -0800

    function that inserts a value in a Binary Search Tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,26 +1,23 @@
-#include <stdlib.h>
 #include "binary_trees.h"
-
 /**
- * binary_tree_node - Creates a binary tree node.
- * @parent: A pointer to the parent of the node to create.
- * @value: The value to put in the new node.
- *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
+ * binary_tree_node - create a new node
+ * @parent: pointer to parent node
+ * @value: value for new node
+ * Return: pointer to new node or NULL
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+binary_tree_t *node;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
-		return (NULL);
+node = malloc(sizeof(binary_tree_t));
+if (node == NULL)
+{
+return (NULL);
+}
+node->parent = parent;
+node->n = value;
+node->left = NULL;
+node->right = NULL;
 
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
-
-	return (new);
+return (node);
 }
