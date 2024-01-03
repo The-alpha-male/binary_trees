0eb39b5a743a49ad6d804feb54847f70e748a1ba
The-alpha-male <ronnymunene@outlook.com>
2024-01-03 12:33:30 -0800

    function that goes through a binary tree using level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,6 +1,44 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height of
+ *
+ * Return: the height of the tree. If tree is NULL, return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left >= right)
+		return (1 + left);
+	return (1 + right);
+}
+
+/**
+ * binary_tree_level - perform a function on a specific level of a binary tree
+ * @tree: pointer to the root of the tree
+ * @l: level of the tree to perform a function on
+ * @func: function to perform
+ *
+ * Return: void
+ */
+void binary_tree_level(const binary_tree_t *tree, size_t l, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	if (l == 1)
+		func(tree->n);
+	else if (l > 1)
+	{
+		binary_tree_level(tree->left, l - 1, func);
+		binary_tree_level(tree->right, l - 1, func);
+	}
+}
 
 /**
  * binary_tree_levelorder - traverses a binary tree using level-order traversal
@@ -10,46 +48,13 @@
  *
  * Return: void
  */
-
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-binary_tree_t **queue, *node;
-size_t front = 0, back = 0, size = 1;
+	size_t height, i;
 
-if (!tree || !func)
-return;
-queue = malloc(sizeof(*queue) * size);
-if (!queue)
-return;
-queue[back] = (binary_tree_t *)tree;
-while (size)
-{
-node = queue[front];
-func(node->n);
-if (node->left)
-{
-if (size == back + 1)
-{
-size <<= 1;
-queue = realloc(queue, sizeof(*queue) * size);
-if (!queue)
-return;
-}
-queue[++back] = node->left;
-}
-if (node->right)
-{
-if (size == back + 1)
-{
-size <<= 1;
-queue = realloc(queue, sizeof(*queue) * size);
-if (!queue)
-return;
-}
-queue[++back] = node->right;
-}
-front++;
-size--;
-}
-free(queue);
+	if (tree == NULL || func == NULL)
+		return;
+	height = binary_tree_height(tree);
+	for (i = 1; i <= height; i++)
+		binary_tree_level(tree, i, func);
 }
