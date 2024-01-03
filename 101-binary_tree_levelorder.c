9f29e8c5e3182bf2ca169f299880bbcd6cbcc57b
The-alpha-male <ronnymunene@outlook.com>
2024-01-03 12:30:27 -0800

    fixed betty issues

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_levelorder - traverses a binary tree using level-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node.
+ * The value in the node must be passed as a parameter to this function
+ *
+ * Return: void
+ */
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
